03551ef3751ff5e86ae8c37caa2d7835e6264f69
miclebrick <miclebrick@outlook.com>
2020-04-25 17:10:26 -0400

    it works

--- app/src/main/java/com/github/miclebrick/usamapapp/MainActivity.kt
+++ app/src/main/java/com/github/miclebrick/usamapapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.github.miclebrick.usamapapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView.setImageBitmap(BitmapFactory.decodeResource(resources, R.drawable.usa_map))
     }
 }
